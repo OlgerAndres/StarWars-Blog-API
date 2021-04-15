24e27a4c7ce0fc92f05bc145f555d942eaef5b93
Olger Garita Torres <olgeragt@gmail.com>
2021-04-15 02:29:12 +0000

    Tabla characters

--- src/models.py
+++ src/models.py
@@ -8,6 +8,8 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -17,3 +19,11 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(120),unique=True,nullable=False)
+    gender = db.Column(db.String(120),unique=True,nullable=False)
+    hair_color =  db.Column(db.String(70),unique=True,nullable=False)
+    eye_color =  db.Column(db.String(70),unique=True,nullable=False)
+    heigth =  db.Column(db.Integer,unique=True,nullable=False)

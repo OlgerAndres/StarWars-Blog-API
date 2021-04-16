672dbcebcdae392b00e755dfa34887018e6e8c80
Olger Garita Torres <olgeragt@gmail.com>
2021-04-16 03:26:22 +0000

    Models, endpoints de user register

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User,Characters,Planets
 #from models import Person
+#from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -19,6 +20,10 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+
+# app.config["JWT_SECRET_KEY"] = "super-secrect"
+# jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -39,15 +44,42 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@app.route('/characters',methods=['GET'])
-def get_characters():
-    response = {'message': 'success'}
-    return jsonify(response)
+@app.route('/register',methods=['POST'])
+def create_register():
+    email = request.json.get("email",None)
+    password = request.json.get('password',None)
 
-@app.route('/planets',methods=['GET'])
+    if email is None:
+        return jsonify({"msg": "No email was provided"}),400
+    if password is None:
+        return jsonify({"msg" : "No email was provided"}),400
+    user = User.query.filter_by(email=email,password=password).first()
+    if user:
+        return  jsonify({"msg" : "User already exists"}),401
+    else:
+        new_user = User()
+        new_user.email = email
+        new_user.password = password
+
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"msg": "User created succesfully"}),200   
+    
+
+
+@app.route('/characters/',methods=['GET'])
+def get_characters():
+    response_body = {
+        "msg": "Hello, this is your GET /characters response "
+    }
+    return jsonify(response_body), 200
+
+@app.route('/planets/',methods=['GET'])
 def get_planets():
-    response = {'message:' 'success'}
-    return jsonify(response)
+  response_body = {
+        "msg": "Hello, this is your GET /planets response "
+    }
+  return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.Boolean(120), unique=False, nullable=False)
 
 class Characters(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -29,8 +30,6 @@ class Planets(db.Model):
 #     characters_id =Column(Integer,ForeignKey('characters.id'))
 #     planets_id = Column(Integer,ForeignKey('planets.id'))
         
-
-
 
     def __repr__(self):
         return '<User %r>' % self.username
